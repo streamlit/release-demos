84a590c75dfd9773ecbe1ede18ea6e0410e387f4
karrie <karrie@streamlit.io>
2020-08-13 17:38:05 -0400

    Cleanup template

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,7 @@
 import streamlit as st
-import pandas as pd
+import validators
 
-try:
-    from releases.release_0_65_features import FEATURES, set_page_config
-except:
-    import urllib.request
-    with urllib.request.urlopen('https://raw.githubusercontent.com/streamlit/release-demos/master/releases/release_0_65_features.py') as response:
-       exec(response.read())
+from __config__ import FEATURES, set_page_config
 
 __VERSION__ = "0.65.0"
 
@@ -22,12 +17,14 @@ feature = st.sidebar.radio(
 # st.sidebar.markdown("")
 
 st.sidebar.header("Callouts")
-st.sidebar.markdown("Deprecation Warning: The `st.image` parameter `format` has been renamed to `output_format`")
+st.sidebar.markdown("""
+Deprecation Warning: The `st.image` parameter `format` has been renamed to `output_format`
+""")
 
 st.sidebar.markdown("----")
 st.sidebar.markdown("""
 Check out our
-[github](https://github.com/streamlit/streamlit/compare/0.64.0...0.64.1.dev20200811)
+[github](https://github.com/streamlit/streamlit/compare/0.64.0...0.65.0)
 to see the full list of changes or join us on our
 [forum](https://discuss.streamlit.io/c/official-announcements/6) for any discussions!
 """)
@@ -36,8 +33,13 @@ st.header("Introducing {0}".format(feature))
 st.write(FEATURES[feature]["text"])
 if ("docs" in FEATURES[feature]):
     st.write("See our [docs]({0}) for more information!".format(FEATURES[feature]["docs"]))
+
 code = FEATURES[feature]["code"]
-if (feature != "Page Configurations [beta]"):
+if (code.endswith('.py')):
+    with open("demos/{1}".format(__VERSION__.replace('.', '_'), code), 'r') as f:
+        code = f.read()
+
+if ("execute" in FEATURES[feature] and FEATURES[feature]["execute"]):
     exec(code)
 st.subheader("Example:")
 st.code(code, language="python")
