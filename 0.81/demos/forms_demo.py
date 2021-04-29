c917fb99f6603b51e3e90cc7138e8c39d6987820
Johannes Rieke <johannes.rieke@gmail.com>
2021-04-29 15:55:03 +0200

    Change traingenerator link

--- 0.81/demos/forms_demo.py
+++ 0.81/demos/forms_demo.py
@@ -23,34 +23,106 @@ apps = [
         "Daniel Lewis",
         "https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py",
     ),
-    ("Traingenerator", "Johannes Rieke", "https://traingenerator.jrieke.com/"),
+    (
+        "Traingenerator",
+        "Johannes Rieke",
+        "https://share.streamlit.io/jrieke/traingenerator/s4a/app/main.py",
+    ),
 ]
 
 components = [
     ("3dmol", "José Manuel Nápoles", "https://github.com/napoles-uach/streamlit_3dmol"),
     ("Ace", "Okld", "https://share.streamlit.io/okld/streamlit-ace/demo/"),
-    ("AgGrid", "Pablo Fonseca", "https://share.streamlit.io/pablocfonseca/streamlit-aggrid/main/examples/example.py"),
-    ("Agraph", "Christian Klose", "https://share.streamlit.io/chrischross/inspirationals/main/main.py"),
+    (
+        "AgGrid",
+        "Pablo Fonseca",
+        "https://share.streamlit.io/pablocfonseca/streamlit-aggrid/main/examples/example.py",
+    ),
+    (
+        "Agraph",
+        "Christian Klose",
+        "https://share.streamlit.io/chrischross/inspirationals/main/main.py",
+    ),
     ("Annotated Text", "Thiago Teixeira", "https://github.com/tvst/st-annotated-text"),
-    ("Bokeh Events", "Ashish Shukla", "https://github.com/ash2shukla/streamlit-bokeh-events"),
-    ("Cropper", "Turner Anderson", "https://github.com/turner-anderson/streamlit-cropper"),
-    ("D3 Demo", "Fanilo Andrianasolo", "https://github.com/andfanilo/streamlit-d3-demo"),
-    ("Discourse", "Okld", "https://share.streamlit.io/okld/streamlit-discourse-demo/main/app.py"),
-    ("Disqus", "Okld", "https://share.streamlit.io/okld/streamlit-disqus-demo/main/app.py"),
-    ("Drawable Canvas", "Fanilo Andrianasolo", "https://share.streamlit.io/andfanilo/streamlit-drawable-canvas-demo/master/app.py"),
-    ("Echarts", "Fanilo Andrianasolo", "https://share.streamlit.io/andfanilo/streamlit-echarts-demo/master/app.py"),
-    ("Embedcode", "Randy Zwitch", "https://share.streamlit.io/randyzwitch/streamlit-embedcode/examples/streamlit_app.py"),
-    ("Folium", "Randy Zwitch", "https://share.streamlit.io/randyzwitch/streamlit-folium/examples/streamlit_app.py"),
+    (
+        "Bokeh Events",
+        "Ashish Shukla",
+        "https://github.com/ash2shukla/streamlit-bokeh-events",
+    ),
+    (
+        "Cropper",
+        "Turner Anderson",
+        "https://github.com/turner-anderson/streamlit-cropper",
+    ),
+    (
+        "D3 Demo",
+        "Fanilo Andrianasolo",
+        "https://github.com/andfanilo/streamlit-d3-demo",
+    ),
+    (
+        "Discourse",
+        "Okld",
+        "https://share.streamlit.io/okld/streamlit-discourse-demo/main/app.py",
+    ),
+    (
+        "Disqus",
+        "Okld",
+        "https://share.streamlit.io/okld/streamlit-disqus-demo/main/app.py",
+    ),
+    (
+        "Drawable Canvas",
+        "Fanilo Andrianasolo",
+        "https://share.streamlit.io/andfanilo/streamlit-drawable-canvas-demo/master/app.py",
+    ),
+    (
+        "Echarts",
+        "Fanilo Andrianasolo",
+        "https://share.streamlit.io/andfanilo/streamlit-echarts-demo/master/app.py",
+    ),
+    (
+        "Embedcode",
+        "Randy Zwitch",
+        "https://share.streamlit.io/randyzwitch/streamlit-embedcode/examples/streamlit_app.py",
+    ),
+    (
+        "Folium",
+        "Randy Zwitch",
+        "https://share.streamlit.io/randyzwitch/streamlit-folium/examples/streamlit_app.py",
+    ),
     ("HiPlot", "Facebook Research", "https://github.com/facebookresearch/hiplot"),
     ("Jina", "Jina", "https://github.com/jina-ai/streamlit-jina"),
-    ("Lottie", "Fanilo Andrianasolo", "https://share.streamlit.io/andfanilo/streamlit-lottie-demo/master/app.py"),
-    ("Observable", "Alex Garcia", "https://streamlit-observable.herokuapp.com/?section=Introduction"),
+    (
+        "Lottie",
+        "Fanilo Andrianasolo",
+        "https://share.streamlit.io/andfanilo/streamlit-lottie-demo/master/app.py",
+    ),
+    (
+        "Observable",
+        "Alex Garcia",
+        "https://streamlit-observable.herokuapp.com/?section=Introduction",
+    ),
     ("Pandas Profiling", "Okld", "https://github.com/okld/streamlit-pandas-profiling"),
-    ("Player", "Okld", "https://share.streamlit.io/okld/streamlit-player-demo/main/app.py"),
-    ("spaCy", "Explosion", "https://share.streamlit.io/ines/spacy-streamlit-demo/master/app.py"),
-    ("Terran Timelines", "Pento Group", "https://github.com/pento-group/streamlit-terran-timeline/"),
+    (
+        "Player",
+        "Okld",
+        "https://share.streamlit.io/okld/streamlit-player-demo/main/app.py",
+    ),
+    (
+        "spaCy",
+        "Explosion",
+        "https://share.streamlit.io/ines/spacy-streamlit-demo/master/app.py",
+    ),
+    (
+        "Terran Timelines",
+        "Pento Group",
+        "https://github.com/pento-group/streamlit-terran-timeline/",
+    ),
     ("Vega-Lite", "Dominik Moritz", "https://github.com/domoritz/streamlit-vega-lite"),
-    ("WebRTC", "Yuichiro Tachibana", "https://share.streamlit.io/whitphx/streamlit-webrtc-example/main/app.py"),
+    (
+        "WebRTC",
+        "Yuichiro Tachibana",
+        "https://share.streamlit.io/whitphx/streamlit-webrtc-example/main/app.py",
+    ),
 ]
 
 
