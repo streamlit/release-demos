f5504fbf3d113ba73bf659299df5dabe72148970
Johannes Rieke <johannes.rieke@gmail.com>
2021-08-09 20:01:02 +0200

    Add app for 0.87

---
+++ 0.87/demos/st_metric.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+
+def show():
+    st.image(
+        "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/slot-machine_1f3b0.png",
+        width=100,
+    )
+    st.write(
+        """
+        # Try out `st.metric`!
+
+        It lets you to display a number in big bold font in your app. Really useful 
+        for KPIs or other important metrics.
+        """
+    )
+
+    st.metric("Streamlit version", 0.87, "0.01")
+
+    st.write("---")
+    st.write("`st.metric` looks especially nice in combination with `st.columns`:")
+    col1, col2, col3 = st.beta_columns(3)
+    col1.metric("Metric 1", "1.2k", "5%")
+    col2.metric("Metric 2", 456, "-8%")
+    col3.metric("Metric 2", 0.75, "0.12")
+
+    st.write("---")
+    st.write("Want to use it? Have a look in our [docs]()!")
+
+
+if __name__ == "__main__":
+    show()
---
+++ 0.87/streamlit_app.py
@@ -0,0 +1,84 @@
+import sys
+from pathlib import Path
+
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
+try:
+    sys.path.remove(str(parent))
+except ValueError:  # Already removed
+    pass
+
+import streamlit as st
+from util.release_helper import create_static_notes
+
+VERSION = ".".join(st.__version__.split(".")[:2])
+
+from demos import st_metric
+
+previous_version = "0.86.0"
+demo_pages = {
+    "st.metric": st_metric.show,
+}
+
+st.set_page_config(page_title=f"New features in Streamlit {VERSION}")
+
+contributors = []
+
+intro = f"""
+This release launches ...
+"""
+
+release_notes = f"""
+---
+**Highlights**
+
+- ...
+
+**Notable Changes**
+
+- ...
+
+**Other Changes**
+
+- ...
+
+"""
+# End release updates
+
+
+def draw_main_page():
+    st.write(
+        f"""
+        # Welcome to Streamlit {VERSION}! 👋
+        """
+    )
+
+    st.write(intro)
+
+    st.write(release_notes)
+
+    create_static_notes(contributors, previous_version, VERSION)
+
+
+# Draw sidebar
+pages = list(demo_pages.keys())
+
+if len(pages):
+    pages.insert(0, "Release Notes")
+    st.sidebar.title(f"Streamlit v{VERSION} Demos")
+    query_params = st.experimental_get_query_params()
+    if "page" in query_params and query_params["page"][0] == "headliner":
+        idx = 1
+    else:
+        idx = 0
+    selected_demo = st.sidebar.radio("", pages, idx)
+else:
+    selected_demo = "Release Notes"
+
+# Draw main page
+if selected_demo in demo_pages:
+    demo_pages[selected_demo]()
+else:
+    draw_main_page()
