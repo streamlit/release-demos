1bc3b6ff51e69b7a9ee3a2b3d9dc8a7699dd4746
asaini <abhi@streamlit.io>
2021-08-19 13:23:13 -0700

    PR feedback + release notes + add dataset

--- 0.87/demos/st_metric.py
+++ 0.87/demos/st_metric.py
@@ -3,6 +3,11 @@ import pandas as pd
 import altair as alt
 
 
+@st.cache
+def load_dataset(file_name):
+    return pd.read_csv(file_name)
+
+
 def show():
     st.image(
         "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/slot-machine_1f3b0.png",
@@ -20,7 +25,7 @@ def show():
     st.subheader('API')
     st.write("Here's an example which shows the API and `st.metric` in action")
     with st.echo():
-        st.metric(label="Streamlit version", value=0.87, delta="0.01")
+        st.metric(label="Streamlit version", value=0.87, delta=0.01)
 
     st.write("---")
     st.subheader("Use in columns")
@@ -35,13 +40,10 @@ def show():
     st.write("---")
 
     st.write("Let's look at some data from the Iris Dataset")
-    # st.subheader('Iris Dataset')
-
-    iris = pd.read_csv('iris.csv')
+    iris = load_dataset('iris.csv')
 
     avg_sepal_length = iris['sepallength'].mean()
     avg_sepal_width = iris['sepalwidth'].mean()
-
 
     with st.echo():
         col1, col2 = st.columns(2)
@@ -73,11 +75,11 @@ def show():
     st.write("---")
 
     st.write('''Sometimes, an increase in a metric can mean a bad thing. 
-    And a decrease in a metric can be a good. Simply use `delta_color="inverse"` to invert colors for ⬆️ and ⬇️''')
+    And a decrease in a metric can be good. Simply use `delta_color="inverse"` to invert colors for ⬆️ and ⬇️''')
     with st.echo():
         col1, col2, col3 = st.columns(3)
-        col1.metric("Average Query Latency", "1.99ms", "0.25ms", delta_color='inverse')
-        col2.metric("Median Script Execution Time", "0.55ms", "-0.15", delta_color='inverse')
+        col1.metric("Average Query Latency", "1.99ms", "0.25ms", delta_color="inverse")
+        col2.metric("Median Script Execution Time", "0.55ms", "-0.15", delta_color="inverse")
 
     st.write("---")
     st.write("Want to use `st.metric`? Have a look in our [docs]() for the full API details!")
