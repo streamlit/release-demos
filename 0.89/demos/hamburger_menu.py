7cec26a23aaf62b41f08be21e1d781d3e825feea
willhuang1997 <willhuang1997@gmail.com>
2021-09-21 16:50:03 -0700

    hamburger demo

--- 0.89/demos/hamburger_menu.py
+++ 0.89/demos/hamburger_menu.py
@@ -1,4 +1,28 @@
 import streamlit as st
 
-def show():
-    st.title("Hamburger Menu")
+st.markdown(" ## Hamburger Menu Customization!")
+st.write("-------------------------------------------------------------------------------")
+
+st.write("Here is the new, original hamburger menu without customization in developer mode.")
+st.image("original.png")
+st.markdown("**As one can see, there is now a developer menu in grey in developer mode!**")
+st.write("-------------------------------------------------------------------------------")
+
+st.write("Here is the new, original hamburger menu without customization as a viewer.")
+st.image("original_viewer.png")
+st.write("-------------------------------------------------------------------------------")
+
+st.write(" If one wants to show information about their app, one can do that through this feature!")
+st.image("about.jpeg", "This is an example!")
+
+st.code("""about_info = ''' ## My Custom App 
+This app uses our ML model to demostrate churn prediction '''
+menu_items = {'About': about_info}
+st.set_page_config(menu_items=menu_items)""")
+st.write("-------------------------------------------------------------------------------")
+
+st.write("If one wants to override links, one can do that through the `'Get help'` or `'Report a bug'` key")
+st.code("""
+get_help_link = https://support.google.com/googlenest/troubleshooter/7211062?hl=en
+menu_items = {'Get help': get_help_link}
+st.set_page_config(menu_items=menu_items) """)
