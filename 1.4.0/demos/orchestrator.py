438c760cc10d990685a43aedc91624cf1bdcfc48
Johannes Rieke <johannes.rieke@gmail.com>
2022-01-13 18:35:29 +0100

    Adapt description, put filter below camera input

--- 1.4.0/demos/orchestrator.py
+++ 1.4.0/demos/orchestrator.py
@@ -7,8 +7,15 @@ from . import examples
 def show_examples():
 
     st.write(
-            """
-        # Camera Input Demo
+        """
+        # 📸 st.camera_input
+        
+        We are launching a new widget, which lets the user take an image through their 
+        webcam and upload it with the app! Use it with:
+        
+        ```python
+        img_file = st.camera_input("Take a picture")
+        ```
         """
     )
 
