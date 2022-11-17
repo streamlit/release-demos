efa79f303730217ef2487cc750655cb1ef47b33f
Maya Barnes <maya8617@gmail.com>
2022-11-17 10:55:56 -0800

    Revise label markdown demo

--- 1.15.0/label-markdown/streamlit_app.py
+++ 1.15.0/label-markdown/streamlit_app.py
@@ -8,16 +8,16 @@ st.title(":gear: Widget Label Markdown Demo")
 col1, col2 = st.columns(2, gap="large")
 text_contents = '''This is some text'''
 
-plain_table = """ Table:
-| Syntax | Description || ----------- | ----------- || Header | Title || Paragraph | Text |
-"""
+# plain_table = """ Table:
+# | Syntax | Description || ----------- | ----------- || Header | Title || Paragraph | Text |
+# """
 
-table = """ Table:
-| Syntax | Description |
-| ----------- | ----------- |
-| Header | Title |
-| Paragraph | Text |
-"""
+# table = """ Table:
+# | Syntax | Description |
+# | ----------- | ----------- |
+# | Header | Title |
+# | Paragraph | Text |
+# """
 
 plain_long_title = """
 How would you like to be contacted?
@@ -32,70 +32,95 @@ What's your favorite `code` language? :computer:
 **Pick** a *Color*: :rainbow:
 """
 
-corgi = Image.open("1.15.0/label-markdown/corgi.jpeg")
-
-img_title = "This is an image: ![Corgi](corgi)"
+# corgi = Image.open("corgi.jpeg")
+# img_title = "This is an image: ![Corgi](corgi)"
 
 
 with col1:
     st.header("Regular Labels:")
+    st.write("Previously, no markdown enabled :cry:")
+    st.markdown("""---""")
+
+    st.subheader("Button Labels:")
+    st.write("`st.button` & `st.download_button` without markdown (emoji icons work, shortcodes not supported):")
+    st.button("Go Button")
+    st.download_button('Download random text', text_contents)
+    st.markdown("""---""")
+
+    st.subheader("Widget/Expander/Tab Labels:")
+    st.write("[Widgets](https://docs.streamlit.io/library/api-reference/widgets), `st.metric`, `st.expander` & `st.tabs` without markdown")
     st.color_picker('Instructions: Pick a Color:', '#3E55CB')
     st.selectbox("How would you like to be contacted?",
         ("Corgi Stampede", "Home phone", "Mobile phone"))
-    st.metric(label="Temperature", value="105 °F", delta=None)
     st.checkbox("Agree to the thing")
     st.text_input('Share a thought:', 'Donations welcome')
-    st.download_button('Download random text', text_contents)
     st.radio( "What's your favorite coding language?",
         ('Python', 'Javascript', 'Ruby'))
     st.multiselect(
     'I need to highlight these very important words',
     ['Coffee', 'Puppies', 'Doritos'])
-    st.button("Go Button")
+    st.metric(label="Temperature", value="105 °F", delta=None)
     tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
-    expand_1 = st.expander("An expander.. with surprises inside")
+    expand_1 = st.expander("Expander.. with surprises inside")
     expand_1.text_area(plain_long_title, disabled=True)
-    expand_1.text_area(plain_table, disabled=True)
-    expand_1.text_area(img_title, disabled=True)
+    # expand_1.text_area(plain_table, disabled=True)
+    # expand_1.text_area(img_title, disabled=True)
+    st.markdown("""---""")
 
 
 with col2:
     st.header("Fancy Labels :nail_care::skin-tone-3::")
-    st.color_picker('Instructions: **Pick** a *Color*: :rainbow:', '#3E55CB')
+    st.write("New, **limited** markdown enabled :smile:")    
+    st.markdown("""---""")
+
+    st.subheader("Button Labels:")
+    st.write("`st.button` & `st.download_button` will allow **bold**, *italics*, ~~strikethrough~~ & emojis (icon or shortcodes!) 😊")
+    st.button("**Stop** Button :traffic_light:")
+    st.download_button('Download random text :page_facing_up:', text_contents)
+    st.markdown("""---""")
+
+    st.subheader("Widget/Expander/Tab Labels:")
+    st.write("[Widgets](https://docs.streamlit.io/library/api-reference/widgets), `st.metric`, `st.expander` & `st.tabs` support **bold**, *italics*, ~~strikethrough~~, emojis, links & code")
+    st.color_picker('**Instructions:** Pick a *Color*: :rainbow:', '#3E55CB')
     st.selectbox("How would you like to be [contacted](https://dictionary.cambridge.org/us/dictionary/english/contacted)?",
         ("Carrier Pidgeon", "Email", "Mobile phone"))
-    st.metric(label="Temperature = *Spicy* :hot_pepper:", value="105 °F", delta=None)
     st.checkbox("Agree to the thing :thumbsup:")
     st.text_input('Share a thought ~~or your credit card number~~:', 'Donations welcome')
-    st.download_button('Download random text :page_facing_up:', text_contents)
     st.radio( "What's your favorite `coding` language? :computer:",
         ('Python', 'Javascript', 'Ruby'))
     st.multiselect(
     'I need to highlight these **very** important words',
     ['Coffee', 'Puppies', 'Golf'])
-    st.button("**Stop** Button :traffic_light:")
+    st.metric(label="Temperature = *Spicy* :hot_pepper:", value="105 °F", delta=None)
     tab1, tab2, tab3 = st.tabs(["**Cat** :cat:", "*Dog* :dog:", "~~Owl~~ :owl:"])
-    expand_2 = st.expander("An **expander**.. with surprises ~~widgets~~ inside :cyclone:")
+    expand_2 = st.expander("**Expander**.. with surprises ~~widgets~~ inside :cyclone:")
     expand_2.text_area(long_title)
-    expand_2.text_area(table)
-    expand_2.text_area(img_title, key=2)
+    # expand_2.text_area(table)
+    # expand_2.text_area(img_title, key=2)
+    st.markdown("""---""")
+
 
 with st.sidebar:
+    st.subheader("Button Labels:")
+    st.button("**Go** Button :white_check_mark:")
+    st.download_button(':arrow_down: Download random text', text_contents)
+    st.markdown("""---""")
+
+    st.subheader("Widget/Expander/Tab Labels:")
     st.color_picker('Instructions: **Pick** a *Color*: :rainbow:', '#BB0329')
     st.selectbox("How would you like to be [contacted](https://dictionary.cambridge.org/us/dictionary/english/contacted)?",
         ("Corgi Stampede", "Email", "Mobile phone"))
-    st.metric(label="Temperature = **Cold** :snowflake:", value="30 °F", delta=None)
     st.checkbox("Agree to the thing :thumbsup::skin-tone-3:")
     st.text_input('Random thought ~~or your credit card number~~:', 'Donations welcome')
-    st.download_button(':arrow_down: Download random text', text_contents)
     st.radio( "What's your favorite `code` language? :computer:",
     ('Python', 'Javascript', 'Ruby'))
     st.multiselect(
     'I need to highlight these **very** important words',
     ['Coffee', 'Puppies', 'Sushi'])
-    st.button("**Go** Button :white_check_mark:")
+    st.metric(label="Temperature = **Cold** :snowflake:", value="30 °F", delta=None)
     tab4, tab5, tab6 = st.tabs(["**Cat** :cat:", "*Dog* :dog:", "~~Owl~~ :owl:"])
-    expand_3 = st.expander("An **expander**.. with surprises ~~widgets~~ inside :cyclone:")
+    expand_3 = st.expander("**Expander**.. with surprises ~~widgets~~ inside :cyclone:")
     expand_3.text_area(long_title, key=7)
-    expand_3.text_area(table, key=8)
-    expand_3.text_area(img_title, key=9)
+    # expand_3.text_area(table, key=8)
+    # expand_3.text_area(img_title, key=9)
+    st.markdown("""---""")
