51ae4115d5da2b51204f9e19d7b789fbdfd96196
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-10-19 12:38:16 -0400

    Update 02_🔗_ st.connection.py

--- 1.28/pages/02_🔗_ st.connection.py
+++ 1.28/pages/02_🔗_ st.connection.py
@@ -11,14 +11,10 @@ st.set_page_config("st.connection demo", "🔗", layout="wide")
 icon("🔗")
 
 st.title("st.connection Demo", anchor=False)
-# st.caption(
-#     """
-#     With `st.connection()`, you can now connect data sources and APIs to your Streamlit app with a fraction of the code.
-#     """
-# )
-st.write("`st.connection` is no longer experimental – it's officially supported! Be sure to switch over from `st.experimental_connection` to `st.connection.` Learn more about [`st.connection`](https://docs.streamlit.io/) and [experimental features](https://docs.streamlit.io/library/advanced-features/prerelease).")
+st.write("`st.connection` is now fully supported in Streamlit. Upgrade your legacy `st.experimental_connection` scripts today to enjoy the benefits of this newly released version. Get started building with [`st.connection`](https://docs.streamlit.io/) by checking out our docs.")
+st.info("Get started building with [`st.connection`](https://docs.streamlit.io/) by checking out our docs.")
+# st.write("`st.connection` is no longer experimental – it's officially supported! Be sure to switch over from `st.experimental_connection` to `st.connection.` Learn more about [`st.connection`](https://docs.streamlit.io/) and [experimental features](https://docs.streamlit.io/library/advanced-features/prerelease).")
 # st.info("You can now switch from **st.experimental_connection** to the more stable and officially supported **st.connection**. Learn more about [st.connection](https://docs.streamlit.io/).", icon="💡")
-# st.write("Learn more about `st.connection` in [<PLACEHOLDER_OUR_DOCS>](https://docs.streamlit.io/).")
 
 tab1, tab2 = st.tabs([
     "🚀 With **st.connection**", 
