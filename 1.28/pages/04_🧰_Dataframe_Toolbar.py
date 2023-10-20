617f922994febd86c4ee122d109f977b7c4d73a6
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-10-20 09:18:28 -0400

    Update 04_🧰_Dataframe_Toolbar.py

--- 1.28/pages/04_🧰_Dataframe_Toolbar.py
+++ 1.28/pages/04_🧰_Dataframe_Toolbar.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import streamlit as st
 
-
 def icon(emoji: str):
     """Shows an emoji as a Notion-style page icon."""
     st.write(
         f'<span style="font-size: 78px; line-height: 1">{emoji}</span>',
         unsafe_allow_html=True,
     )
-
     
 st.set_page_config("Toolbar for dataframes demo", "🧰", layout="wide")
 icon("🧰")
