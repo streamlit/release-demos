e291b61a7d68a5646bf26e9463d60d492c377638
Tony Kipkemboi <117667118+sfc-gh-tkipkemboi@users.noreply.github.com>
2023-11-21 17:09:17 -0500

    Update 01_🚀_Demos.py

--- 1.29/pages/01_🚀_Demos.py
+++ 1.29/pages/01_🚀_Demos.py
@@ -23,7 +23,7 @@ with gif1:
     st.subheader("Old loading message")
     st.caption("Upon reload, the page shows :blue[Please wait...]")
     st.markdown(
-        f'<img src="data:image/gif;base64,{skeleton_url}" width=400 alt="demo gif">',
+        f'<img src="data:image/gif;base64,{skeleton_url}" width=500 alt="demo gif">',
         unsafe_allow_html=True,
     )
 
