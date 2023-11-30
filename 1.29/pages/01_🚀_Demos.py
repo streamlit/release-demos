135634a9aed7f710fa934ae575496c5315a3f710
Tony Kipkemboi <117667118+sfc-gh-tkipkemboi@users.noreply.github.com>
2023-11-30 13:22:11 -0500

    Update 01_🚀_Demos.py

--- 1.29/pages/01_🚀_Demos.py
+++ 1.29/pages/01_🚀_Demos.py
@@ -102,7 +102,7 @@ with before:
         st.button("Refresh data", use_container_width=True, key="before1")
         
     # Instead of st.empty(), use a markdown with empty space
-    st.markdown('<div style="height: 30px;"></div>', unsafe_allow_html=True)
+    st.markdown('<div style="height: 20px;"></div>', unsafe_allow_html=True)
     
     st.code(
         """
