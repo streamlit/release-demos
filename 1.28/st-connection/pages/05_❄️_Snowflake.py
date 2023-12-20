6c690978c9add5989428c7433c08073b7479e21b
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-12-20 13:18:03 -0500

    API updates

--- 1.28/st-connection/pages/05_❄️_Snowflake.py
+++ 1.28/st-connection/pages/05_❄️_Snowflake.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
-from snowflake.snowpark.functions import col
-import pandas as pd
-
 st.set_page_config(
     page_title='Snowflake Connections',
     page_icon='❄️'
@@ -33,16 +30,6 @@ role = "[MYROLE]"
 warehouse = "[MYWAREHOUSE]"
     """, language = "toml")
 
-    "Alternatively, you can use `~/.snowsql/config` (syntax is the same except the header)"
-    st.code("""
-# ~/.snowsql/config
-[connections]
-authenticator = "externalbrowser"
-account = "[MYACCOUNT]"
-user = "[username]"
-role = "[MYROLE]"
-warehouse = "[MYWAREHOUSE]"
-    """, language = "toml")
     """
     *For questions on the required `account` value, see [here](https://docs.snowflake.com/en/user-guide/python-connector-api.html#label-account-format-info).
     Full list of supported parameters [here](https://docs.snowflake.com/en/user-guide/python-connector-api.html#connect).*
@@ -58,7 +45,7 @@ run_the_code = st.checkbox("Try running the code (requires local snowflake creds
 st.subheader("Initialize a connection")
 with st.echo():
     if run_the_code:
-        conn = st.connection('snowpark')
+        conn = st.connection('snowflake')
 
         conn
 
@@ -87,28 +74,17 @@ with st.echo():
         df = conn.query(query)
         st.dataframe(df)
 
-st.subheader("safe_session() for thread safety")
+st.subheader("session() for full operations")
 
-"Use `conn.safe_session()` in a `with` block to get thread safe access to a [Snowpark Session](https://docs.snowflake.com/en/developer-guide/snowpark/reference/python/session.html) for more advanced (and often faster) operations."
+"Use `conn.session()` in a `with` block to access a [Snowpark Session](https://docs.snowflake.com/en/developer-guide/snowpark/reference/python/session.html) for more advanced (and often faster) operations."
 
 "You may want to wrap this in a function with `@st.cache_data` to be even faster!"
 with st.echo():
     if run_the_code:
-        with conn.safe_session() as session:
+        from snowflake.snowpark.functions import col
+        import pandas as pd
+        with conn.session() as session:
             local_df = pd.DataFrame({"OWNER": ["jerry", "barbara", "alex"], "PET": ["fish", "cat", "puppy"], "COUNT": [4, 2, 1]})
             snow_df = session.create_dataframe(local_df)
             snow_df = snow_df.filter(col('COUNT') > 1)
             st.dataframe(snow_df)
-
-
-st.subheader("session for full operations")
-"You can also use `conn.session` to get direct access to the Snowpark Session, in this case you are responsible for handling thread safety."
-
-"You may want to wrap this in a function with `@st.cache_data` to be even faster!"
-with st.echo():
-    if run_the_code:
-        sess = conn.session
-        local_df = pd.DataFrame({"OWNER": ["jerry", "barbara", "alex"], "PET": ["fish", "cat", "puppy"], "COUNT": [4, 2, 1]})
-        snow_df = sess.create_dataframe(local_df)
-        snow_df = snow_df.filter(col('COUNT') > 1)
-        st.dataframe(snow_df)
