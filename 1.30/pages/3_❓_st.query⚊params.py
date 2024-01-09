b20d66085c7e0fd6eb06f6ed877507a2df3bfed8
Tony Kipkemboi <117667118+sfc-gh-tkipkemboi@users.noreply.github.com>
2024-01-09 16:11:40 -0500

    Update 3_❓_st.query⚊params.py

--- 1.30/pages/3_❓_st.query⚊params.py
+++ 1.30/pages/3_❓_st.query⚊params.py
@@ -14,12 +14,22 @@ st.title("Demo of st.query_params", anchor=False)
 
 st.divider()
 
+"""
+#### Instructions:
+To see the UTM source printed on this page from query params:
+1. Append `?utm_source=LinkedIn` (or your desired UTM source like X, Instagram, Discord,..) to the URL.
+2. For example, use `https://release130.streamlit.app?utm_source=LinkedIn`.
+3. Hit **Enter** to see the UTM source printed below.
+"""
+
+st.divider()
+
 # Get the current query parameters
 params = st.query_params
 
 utm_source = params.get('utm_source', None) 
 
 if utm_source:
-    st.info(f"UTM Source: {utm_source}")
+    st.markdown(f"**UTM Source**: {utm_source}")
 else:
     st.warning("UTM Source not found in URL.", icon="⚠️")
