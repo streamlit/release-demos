6aba6d361f73cd940de4c12f77d40cfe64b45f7e
Joshua Carroll <joshua.carroll@snowflake.com>
2024-04-17 16:24:36 -0700

    Updates

--- previews/mpa-v2/movies.py
+++ previews/mpa-v2/movies.py
@@ -6,13 +6,13 @@ import pathlib
 
 CURRENT_DIR = pathlib.Path(__file__).parent.resolve()
 
-# st.title('📊 Interactive Data Explorer')
+st.subheader('📊 Interactive Data Explorer')
 
-# with st.expander('About this app'):
-#   st.markdown('**What can this app do?**')
-#   st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-#   st.markdown('**How to use the app?**')
-#   st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
+with st.expander('About this page'):
+  st.markdown('**What can this app do?**')
+  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
+  st.markdown('**How to use the app?**')
+  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
   
 st.subheader('Which Movie Genre performs ($) best at the box office?')
 
